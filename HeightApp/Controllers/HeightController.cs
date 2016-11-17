0edc13c2646345513996b21c9a4fb690c6bfed03
Paul Kenny <paulkenny2@hotmail.com>
2016-11-17 11:57:10 +0000

    fffvf

---
+++ HeightApp/Controllers/HeightController.cs
@@ -0,0 +1,30 @@
+﻿using HeightApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeightApp.Controllers
+{
+    public class HeightController : Controller
+    {
+        // GET: Convert
+        [HttpGet]
+        public ActionResult Convert()
+        {
+            //Height h = new Height { Feet = 0, Inches = 0 };
+            // return View(h);
+            return View();
+        }
+
+        [HttpPost]
+        // GET: Convert
+        public ActionResult Convert(Height n)
+        {                  
+            return View(n);
+        }
+
+
+    }
+}
---
+++ HeightApp/Models/Height.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HeightApp.Models
+{
+    public class Height
+    {
+        const double InchesToMeters = 0.0254;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Feet must be > 0")]
+        public int Feet { get; set; }
+
+        [Range(0, 11, ErrorMessage = "Inches must be 0...11")]
+        public int Inches { get; set; }
+
+        public double Metric
+        {
+            get
+            {
+                return (Feet * 12 + Inches) * InchesToMeters;
+            }
+        }
+    }
+}
